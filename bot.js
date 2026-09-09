bea154f7cf5d1c40f4e2cdd5f4d4ab85fc564f8f
dimon733133kz-srg <dimon733133kz@gmail.com>
2026-09-09 16:12:53 +0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -108,7 +108,7 @@ const rest = new REST({ version: '10' }).setToken(TOKEN);
 async function registerCommands() {
     try {
         await rest.put(
-            Routes.applicationGuildCommands(CLIENT_ID),
+            Routes.applicationCommands(CLIENT_ID),
             { body: commands }
         );
 
